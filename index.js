11105ed765a2075c41bcbaa0a550a155821f0f29
Charles Cusson <charlescusson@gmail.com>
2020-04-16 02:46:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ array.forEach((element, index) => {console.log(`${index}: ${element}`)})
 
 iterate(callback) {
 
-array2 = [goal]
-array2.forEach(callback)
-return array
+array2 = [goal];
+array2.forEach(callback);
+return array;
 
 }
