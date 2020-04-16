d8007dd3129dae7b6931c12a126a4d721ccee17e
Charles Cusson <charlescusson@gmail.com>
2020-04-16 02:20:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function iterativeLog(array){
 array.forEach((element, index) => {console.log(`${index}: ${element}`)})
 
 }
+
+iterate(callback) {
+
+array = [goal]
+array.forEach(callback)
+return array
+  
+}
